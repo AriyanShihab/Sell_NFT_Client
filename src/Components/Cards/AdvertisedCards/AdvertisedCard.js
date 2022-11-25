c76fe033b6b22be01e050d37026f8e564f5245f9
AriyanShihab <devshihab111@gmail.com>
2022-11-25 22:43:53 +0600

    report product done

--- src/Components/Cards/AdvertisedCards/AdvertisedCard.js
+++ src/Components/Cards/AdvertisedCards/AdvertisedCard.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { FaUserCheck } from "react-icons/fa";
+import { toast } from "react-toastify";
+
+import SmallSpinner from "../../Loader/SmallSpinner";
 
 const AdvertisedCard = ({ product, setCurrentProduct, setisModalClose }) => {
+  const [reportLoading, setReportLoading] = useState(false);
+  const [reportText, setReportText] = useState("Report Product");
   const {
     name,
     img,
@@ -12,13 +17,49 @@ const AdvertisedCard = ({ product, setCurrentProduct, setisModalClose }) => {
     description,
     conditionType,
     sellerverified,
+    reported,
   } = product;
+
+  const handelReport = () => {
+    setReportLoading(true);
+    fetch(`http://localhost:5000/add-report/${product._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          toast.info("product reported");
+          setReportLoading(false);
+          setReportText("Alredy reported");
+        }
+      })
+      .catch((err) => {
+        toast.error(err.message);
+        setReportLoading(true);
+      });
+  };
 
   const converSellerVerified = "" + sellerverified;
   return (
     <div>
       <div className=" h-[670px] rounded border border-indigo-300 border-opacity-25 flex flex-col justify-between text-slate-200">
-        <img className="w-full h-[260px] rounded-t" src={img} alt="" />
+        <div className="relative">
+          <img className="w-full h-[260px] rounded-t" src={img} alt="" />
+          {!reported ? (
+            <h2
+              onClick={handelReport}
+              className="absolute bottom-2 right-1 p-1 rounded bg-red-500 text-slate-900 text-bold text-sm cursor-pointer text-center"
+            >
+              {reportLoading ? <SmallSpinner></SmallSpinner> : reportText}
+            </h2>
+          ) : (
+            <>
+              <h2 className="absolute bottom-2 right-1 p-1 rounded bg-red-400 text-slate-900 text-bold text-sm cursor-pointer">
+                Alredy reported
+              </h2>
+            </>
+          )}
+        </div>
         <div className="p-3">
           <h3 className="text-xl my-2 font-bold text-cyan-500">{name}</h3>
           <div className="flex justify-start gap-2">
