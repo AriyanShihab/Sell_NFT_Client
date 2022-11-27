0b2b9c422c7f741a16ff326d4ab9bfba8907c1e0
AriyanShihab <devshihab111@gmail.com>
2022-11-27 22:27:10 +0600

    footer added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,9 +3,30 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="text-center  bottom-0 w-full py-4 bg-cyan-500">
-      This is footer
-      <Link to="/dashbord">Dashsbord</Link>
+    <div className="text-center   py-8 bg-gray-900 border-t border-slate-100">
+      <footer className="p-4  md:flex md:items-center md:justify-between md:p-6 max-w-6xl mx-auto">
+        <span className="text-sm text-gray-100 sm:text-center">
+          © 2022{" "}
+          <a href="https://flowbite.com/" className="hover:underline">
+            Sell NFT™
+          </a>
+          . All Rights Reserved.
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-100 sm:mt-0">
+          <li>
+            <Link className="mr-4 hover:underline md:mr-6 ">About</Link>
+          </li>
+          <li>
+            <Link className="mr-4 hover:underline md:mr-6">Privacy Policy</Link>
+          </li>
+          <li>
+            <Link className="mr-4 hover:underline md:mr-6">Licensing</Link>
+          </li>
+          <li>
+            <Link className="hover:underline">Contact</Link>
+          </li>
+        </ul>
+      </footer>
     </div>
   );
 };
